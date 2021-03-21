483b0aefa5e55fd0d6989d673cda4c3279eb6658
Adnan Sarker <adnansarker92@gmail.com>
2021-03-21 23:49:38 +0600

    config all button and create a state where  audio will be play

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,15 +6,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        //print ("I Got Pressed")
+        let x = sender.currentTitle!
+        playSound(soundName: x)
+        //print ("ok, I played  ", x)
+    }
     
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 
 }
 
